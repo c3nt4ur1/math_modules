8fad3e9ab5588fb7aef3025c367ab01e573b3d14
ANBonin <adriano.bonin@outlook.com>
2022-07-04 21:55:45 -0300

    Created the conversions from celsius degrees and between kelvin and fahrenheit

--- conversions/temperature.h
+++ conversions/temperature.h
@@ -20,15 +20,35 @@ double fahrenheitToCelsius(double temp){
 }
 double kelvinToCelsius(double temp){
     WATER_FREEZING_TEMP_TARGET 0;
-    WATER_FREEZING_TEMP_SOURCE -273.15;
+    WATER_FREEZING_TEMP_SOURCE 273.15;
     return temp - 273.15;
 }
 
 /*
  * FROM CELSIUS
  */
-
-
-
+double celsiusToFahrenheit(double temp){
+    WATER_FREEZING_TEMP_SOURCE 0;
+    WATER_FREEZING_TEMP_TARGET 32;
+    return ((9/5)*temp) - 32;
+}
+double celsiusToKelvin(double temp){
+    WATER_FREEZING_TEMP_SOURCE 0;
+    WATER_FREEZING_TEMP_TARGET 273.15;
+    return temp + 273.15;
+}
+/*
+ * OTHER
+ */
+double kelvinToFahrenheit(double temp){
+    WATER_FREEZING_TEMP_SOURCE 273.15;
+    WATER_FREEZING_TEMP_TARGET 32;
+    return ((9*(temp - 273.15)) / 5) + 32;
+}
+double fahrenheitToKelvin(double temp){
+    WATER_FREEZING_TEMP_SOURCE 32;
+    WATER_FREEZING_TEMP_TARGET 273.15;
+    return ((5*(temp -32)) / 9) + 273.15;
+}
 
 #endif //MATH_ALGORITHMS_TEMPERATURE_H
