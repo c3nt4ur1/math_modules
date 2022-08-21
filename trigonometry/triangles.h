c03c0c095f4ca4009ff1afc6212c26588322abab
ANBonin <adriano.bonin@outlook.com>
2022-08-20 22:21:53 -0300

    Updated comments

--- trigonometry/triangles.h
+++ trigonometry/triangles.h
@@ -22,15 +22,18 @@ namespace pythagoras {
     }
 }
 template<typename T>
-T cosineLawDeg(T a, T b, T alphaDeg){
-    T alpha = (M_PI * alphaDeg) / 180;
+T cosineLawDeg(T a, T b, T alphaDeg){  //Cosine Law with alpha in degrees
+    T alpha = (M_PI * alphaDeg) / 180; //Conversion from degree to radian -> cos() function's param requires val in rad
     T c2 = pow(a, 2) + pow(b, 2) - (2*a*b* cos(alpha));
     return sqrt(c2);
 }
 
 template<typename T>
-T cosineLawRad(T a, T b, T alpha){
+T cosineLawRad(T a, T b, T alpha){ //Cosine Law with alpha in radian
     return sqrt(pow(a, 2) + pow(b, 2) - (2*a*b* cos(alpha)));
 }
+/*
+ * END OF PENDING TESTING AREA
+ */
 
 #endif //MATH_ALGORITHMS_TRIANGLES_H
