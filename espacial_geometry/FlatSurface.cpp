230c1e33f37247afcdd7b518e468c1560a3ec0da
ANBonin <adriano.bonin@outlook.com>
2024-01-29 17:51:36 -0300

    initial definitions of surface

--- espacial_geometry/FlatSurface.cpp
+++ espacial_geometry/FlatSurface.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "FlatSurface.h"
+FlatSurface::FlatSurface(Plane& plane, int sides, vector<float> sidesSizes, vector<float> angles){
+    if(sidesSizes.size() > sides){
+        throw std::invalid_argument("To many size values");
+    }else{
+        this->region = plane;
+        this->sides = sides;
+        this->sidesSizes = sidesSizes;
+        this->angles = angles;
+    }
+}
--- espacial_geometry/FlatSurface.h
+++ espacial_geometry/FlatSurface.h
@@ -5,9 +5,24 @@
 #ifndef MATH_ALGORITHMS_FLATSURFACE_H
 #define MATH_ALGORITHMS_FLATSURFACE_H
 
+#include <vector>
+#include <iostream>
+using namespace std;
 
+#include "plane.h"
 class FlatSurface {
+protected:
+    Plane region;
+    vector<float>position;
 
+    int sides = -1;
+    vector<float> sidesSizes; // counted clockwise
+    vector<float> angles; //counted clockwise, starting from between sides 1 and 2
+
+    float perimeter = NULL;
+public:
+    float area = NULL;
+    FlatSurface(Plane& plane, int sides, vector<float> sidesSizes, vector<float> angles);
 };
 
 
