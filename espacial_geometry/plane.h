04c05381cb104fa10561101d2c1695d6933f3e62
ANBonin <adriano.bonin@outlook.com>
2024-01-29 17:18:51 -0300

    created a 2D vector

---
+++ espacial_geometry/plane.h
@@ -0,0 +1,21 @@
+//
+// Created by ANBonin on 09/01/2024.
+//
+
+#ifndef MATH_ALGORITHMS_PLANE_H
+#define MATH_ALGORITHMS_PLANE_H
+
+#include<vector>
+
+using namespace std;
+
+class Plane {
+protected:
+    vector<int> diagram; //must be seen as a 2D vector
+    /*
+     * each element of the diagram is a dot in a cartesian diagram
+     */
+};
+
+
+#endif //MATH_ALGORITHMS_PLANE_H
